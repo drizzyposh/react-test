b61a0af48b85a851a24619fc6fbf9e4bcf131dc3
Femi Fadayomi <femzyfadayomi@gmail.com>
2023-02-20 22:20:28 +0100

    Jest

--- src/components/greet/greet.test.jsx
+++ src/components/greet/greet.test.jsx
@@ -2,14 +2,21 @@ import { render, screen } from "@testing-library/react"
 import Greet from "./greet"
 
 
-test('Greet renders correctly', () => {
-    render(<Greet />)
-    const textElement = screen.getByText("Hello")
-    expect(textElement).toBeInTheDocument()
-})
+describe("Greet", () => {
+    test('renders correctly', () => {
+        render(<Greet />)
+        const textElement = screen.getByText("Hello")
+        expect(textElement).toBeInTheDocument()
+    })
+    
+  
+   
+}) 
 
-test("Greet renders correctly", () => {
-    render(<Greet name="Fadayomi"/> )
-    const textElement = screen.getByText("Hello Fadayomi")
-    expect(textElement).toBeInTheDocument()
+describe("Nested", () => {
+    test("renders correctly", () => {
+        render(<Greet name="Fadayomi"/> )
+        const textElement = screen.getByText("Hello Fadayomi")
+        expect(textElement).toBeInTheDocument()
+    })
 })
